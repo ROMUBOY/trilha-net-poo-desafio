529fccd31778695084b607e74a278ed03fb09bb0
ROMUBOY <romualdoosfilho94@gmail.com>
2023-09-28 14:15:50 -0300

    feat: update Models/Smartphone.cs Models/Nokia.cs Models/Iphone.cs Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "71999999999", modelo: "Empiria", imei: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Spaceship for Newbies");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "11900000000", modelo: "17 pro max turbo", imei: "222222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Fortnite");
